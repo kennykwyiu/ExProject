0bef31c52efe9ce087a4ac45d0dc926898633442
kenny <kenny.kwyiu@gmail.com>
2024-04-12 15:24:11 +0800

    L3 3-7 Stream - filter

--- src/test/java/kenny/stream/StreamOperator.java
+++ src/test/java/kenny/stream/StreamOperator.java
@@ -1,4 +1,27 @@
 package kenny.stream;
 
+import com.alibaba.fastjson.JSON;
+import kenny.lambda.cart.CartService;
+import kenny.lambda.cart.Sku;
+import kenny.lambda.cart.SkuCategoryEnum;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
 public class StreamOperator {
+
+    List<Sku> list;
+
+    @Before
+    public void init() {
+        list = CartService.getCartSkuList();
+    }
+
+    @Test
+    public void filterTest() {
+        list.stream()
+                .filter(sku -> SkuCategoryEnum.BOOKS.equals(sku.getSkuCategory()))
+                .forEach(x -> System.out.println(JSON.toJSONString(x, true)));
+    }
 }
