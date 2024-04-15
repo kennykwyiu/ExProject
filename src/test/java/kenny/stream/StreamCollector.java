198c38053664df61b15a22aef10e2f31f3f04239
kenny <kenny.kwyiu@gmail.com>
2024-04-15 20:31:48 +0800

    L3 3-17 Stream - groupingBy

--- src/test/java/kenny/stream/StreamCollector.java
+++ src/test/java/kenny/stream/StreamCollector.java
@@ -3,19 +3,35 @@ package kenny.stream;
 import com.alibaba.fastjson.JSON;
 import kenny.lambda.cart.CartService;
 import kenny.lambda.cart.Sku;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamCollector {
+    List<Sku> list;
+
+    @Before
+    public void init() {
+        list = CartService.getCartSkuList();
+    }
     @Test
     public  void  toLIst() {
-        List<Sku> list = CartService.getCartSkuList();
 
         List<Sku> result = list.stream().filter(sku -> sku.getTotalPrice() > 100)
                 .collect(Collectors.toList());
 
         System.out.println(JSON.toJSONString(result, true));
     }
+
+    @Test
+    public void group() {
+        Map<Enum, List<Sku>> group = list.stream()
+                .collect(Collectors.groupingBy(sku -> sku.getSkuCategory()));
+
+        System.out.println(JSON.toJSONString(group, true));
+    }
+
 }
