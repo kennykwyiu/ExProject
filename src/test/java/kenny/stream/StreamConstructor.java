7fb0d74277715c7dde35b33dd5111e6b51556968
kenny <kenny.kwyiu@gmail.com>
2024-04-15 20:10:54 +0800

    L3 3-15 Stream - Arrays.stream()

--- src/test/java/kenny/stream/StreamConstructor.java
+++ src/test/java/kenny/stream/StreamConstructor.java
@@ -2,6 +2,8 @@ package kenny.stream;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamConstructor {
@@ -10,4 +12,13 @@ public class StreamConstructor {
         Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
         stream.forEach(System.out::println);
     }
+
+    @Test
+    public void streamFromArray() {
+        int[] numbers = {1,2,3,4,5};
+        IntStream stream = Arrays.stream(numbers);
+        stream.forEach(System.out::println);
+
+    }
+
 }
