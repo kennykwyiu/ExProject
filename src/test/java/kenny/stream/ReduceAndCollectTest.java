08380e888842a368271adf2de544b6a36d6de03b
kenny <kenny.kwyiu@gmail.com>
2024-04-17 20:50:08 +0800

    L3 3-21 Stream - create Order class

--- src/test/java/kenny/stream/ReduceAndCollectTest.java
+++ src/test/java/kenny/stream/ReduceAndCollectTest.java
@@ -1,4 +1,25 @@
 package kenny.stream;
 
+import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+
 public class ReduceAndCollectTest {
+
+    public void reduceTest() {
+        @Data
+        @AllArgsConstructor
+        class Order {
+            private Integer id;
+            private Integer productCount;
+            private Double totalAmount;
+        }
+
+        ArrayList<Order> list = Lists.newArrayList();
+        list.add(new Order(1, 2, 25.12));
+        list.add(new Order(2, 5, 257.23));
+        list.add(new Order(3, 3, 23332.12));
+    }
 }
