c26a01e8538bde10ccd01d493dd23ad04e282a6d
kenny <kenny.kwyiu@gmail.com>
2024-04-18 21:02:03 +0800

    L3 3-23 Stream - collect

--- src/test/java/kenny/stream/ReduceAndCollectTest.java
+++ src/test/java/kenny/stream/ReduceAndCollectTest.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class ReduceAndCollectTest {
 
@@ -45,5 +47,55 @@ public class ReduceAndCollectTest {
                         });
 
         System.out.println(JSON.toJSONString(order, true));
+    }
+
+    @Test
+    public void collectTest() {
+        @Data
+        @AllArgsConstructor
+        class Order {
+            private Integer id;
+            private String account;
+            private Integer productCount;
+            private Double totalAmount;
+        }
+
+        ArrayList<Order> list = Lists.newArrayList();
+        list.add(new Order(1, "Peter", 2, 25.12));
+        list.add(new Order(2, "Peter", 5, 257.23));
+        list.add(new Order(3, "Liz", 3, 23332.12));
+
+        HashMap<String, Order> collect = list.stream()
+                .collect(
+                        () -> {
+                            System.out.println("execute collect stream!!!");
+                            return new HashMap<String, Order>();
+                        },
+                        (HashMap<String, Order> map, Order newOrder) -> {
+                            System.out.println("execute add new elements into collect");
+
+                            String account = newOrder.getAccount();
+                            if (map.containsKey(account)) {
+                                Order order = map.get(account);
+                                order.setProductCount(newOrder.getProductCount() + order.getProductCount());
+                                order.setTotalAmount(newOrder.getTotalAmount() + order.getTotalAmount());
+
+
+                            } else {
+                                map.put(account, newOrder);
+                            }
+                        },
+                        (HashMap<String, Order> map1, HashMap<String, Order> map2) -> {
+                            System.out.println("execute parallel merge");
+
+                            map2.forEach((key, value) -> {
+                                map1.merge(key, value, (order1, order2) -> {
+                                    return new Order(0, key, order1.getProductCount() + order2.getProductCount(),
+                                            order1.getTotalAmount() + order2.getTotalAmount());
+                                });
+                            });
+                        });
+
+        System.out.println(JSON.toJSONString(collect, true));
     }
 }
