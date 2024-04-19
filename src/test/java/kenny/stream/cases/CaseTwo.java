1017c804e4f14714791aabc6de7a360f07fe57cd
kenny <kenny.kwyiu@gmail.com>
2024-04-19 20:58:58 +0800

    L3 3-26 Stream - distinctTag()

--- src/test/java/kenny/stream/cases/CaseTwo.java
+++ src/test/java/kenny/stream/cases/CaseTwo.java
@@ -1,9 +1,11 @@
 package kenny.stream.cases;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.List;
 
@@ -27,5 +29,13 @@ public class CaseTwo {
                 new TagReqDTO("Peter_3", 10),
                 new TagReqDTO("Liz_4", 30),
                 new TagReqDTO("Peter_3", 11));
+    }
+
+    @Test
+    public void distinctTag() {
+        tagListFromReq.stream()
+                .filter(tag -> !tagListFromDB.contains(tag.getName()))
+                .distinct()
+                .forEach(tag -> System.out.println(JSON.toJSONString(tag, true)));
     }
 }
