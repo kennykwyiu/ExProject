05d01de2d23aff1f19e3dc6ebc24ffa4ff5566d2
kenny <kenny.kwyiu@gmail.com>
2024-04-20 12:45:04 +0800

    L3 3-27 Stream - findPermission()

--- src/test/java/kenny/stream/cases/CaseThree.java
+++ src/test/java/kenny/stream/cases/CaseThree.java
@@ -1,10 +1,12 @@
 package kenny.stream.cases;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +48,15 @@ public class CaseThree {
 
         roleList.add(adminRole);
         roleList.add(userRole);
+    }
+
+    @Test
+    public void findPermission() {
+        roleList.stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .peek(permission -> System.out.println("new element: "+permission))
+                        .distinct()
+                        .forEach(permission -> System.out.println(JSON.toJSONString(permission, true)));
     }
 
 }
