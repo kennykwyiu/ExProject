78b7e8d5e5b68c63d4ad079fc83548969e5b3b70
kenny <kenny.kwyiu@gmail.com>
2024-04-20 12:34:32 +0800

    L3 3-27 Stream - init data

--- src/test/java/kenny/stream/cases/CaseThree.java
+++ src/test/java/kenny/stream/cases/CaseThree.java
@@ -1,9 +1,12 @@
 package kenny.stream.cases;
 
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.junit.Before;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CaseThree {
@@ -18,6 +21,31 @@ public class CaseThree {
     @AllArgsConstructor
     class Permission {
         private String name;
+    }
+
+    List<Role> roleList;
+
+    @Before
+    public void init() {
+        roleList = new ArrayList();
+
+        Role adminRole = new Role();
+        List<Permission> adminPermissionList = Lists.newArrayList(
+                new Permission("删除"),
+                new Permission("查看"),
+                new Permission("导出"));
+        adminRole.setPermissions(adminPermissionList);
+
+        Role userRole = new Role();
+        List<Permission> userPermissionList = Lists.newArrayList(
+                new Permission("新建"),
+                new Permission("修改"),
+                new Permission("删除"),
+                new Permission("查看"));
+        userRole.setPermissions(userPermissionList);
+
+        roleList.add(adminRole);
+        roleList.add(userRole);
     }
 
 }
