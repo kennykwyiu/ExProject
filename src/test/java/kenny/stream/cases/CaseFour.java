a3f596f74b948ca8a52b19886317f9bbbb8c94ad
kenny <kenny.kwyiu@gmail.com>
2024-04-21 23:15:14 +0800

    L3 3-28 Stream - selectFromDB()

--- src/test/java/kenny/stream/cases/CaseFour.java
+++ src/test/java/kenny/stream/cases/CaseFour.java
@@ -3,11 +3,22 @@ package kenny.stream.cases;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaseFour {
     @Data
     @AllArgsConstructor
     class Order {
         private Integer orderId;
         private String accountId;
+    }
+
+    public List<Order> selectFromDB(List<String> accountIds) {
+        ArrayList<Order> orderList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            orderList.add(new Order(i, accountIds.get(i % accountIds.size())));
+        }
+        return orderList;
     }
 }
