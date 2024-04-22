b97dd5a2336423a4d3e12a50688cd64c3f9e9307
kenny <kenny.kwyiu@gmail.com>
2024-04-22 21:06:09 +0800

    L3 3-29 Stream - init()

--- src/test/java/kenny/stream/cases/CaseFive.java
+++ src/test/java/kenny/stream/cases/CaseFive.java
@@ -2,9 +2,12 @@ package kenny.stream.cases;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.junit.Before;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CaseFive {
     @Data
@@ -14,5 +17,31 @@ public class CaseFive {
         private LocalDateTime time;
         private Integer count;
         private String type;
+    }
+
+    List<Trade> trades;
+
+    @Before
+    public void init() {
+        trades = new ArrayList<>();
+
+        trades.add(new Trade(new BigDecimal(100),
+                LocalDateTime.now().plusSeconds(1),
+                500, "Company"));
+        trades.add(new Trade(new BigDecimal(101),
+                LocalDateTime.now().plusSeconds(2),
+                1, "individual"));
+        trades.add(new Trade(new BigDecimal(101),
+                LocalDateTime.now().plusSeconds(1),
+                1, "individual"));
+        trades.add(new Trade(new BigDecimal(100),
+                LocalDateTime.now().plusSeconds(1),
+                500, "individual"));
+        trades.add(new Trade(new BigDecimal(100),
+                LocalDateTime.now().plusSeconds(0),
+                2, "individual"));
+        trades.add(new Trade(new BigDecimal(100),
+                LocalDateTime.now().plusSeconds(0),
+                100, "Company"));
     }
 }
