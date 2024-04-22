975d515066e3f62c94de6a345e50714adbe98742
kenny <kenny.kwyiu@gmail.com>
2024-04-22 20:23:47 +0800

    L3 3-28 Stream - queryOrderByAccountIds()

--- src/test/java/kenny/stream/cases/CaseFour.java
+++ src/test/java/kenny/stream/cases/CaseFour.java
@@ -1,10 +1,17 @@
 package kenny.stream.cases;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CaseFour {
     @Data
@@ -20,5 +27,20 @@ public class CaseFour {
             orderList.add(new Order(i, accountIds.get(i % accountIds.size())));
         }
         return orderList;
+    }
+
+    public Map<String, List<Order>> queryOrderByAccountIds(List<String> accountIds) {
+        System.out.println(JSON.toJSONString(selectFromDB(accountIds)));
+        return Optional.ofNullable(selectFromDB(accountIds))
+                .map(List::stream)
+                .orElseGet(Stream::empty)
+                .collect(Collectors.groupingBy(order -> order.getAccountId()));
+    }
+
+    @Test
+    public void test() {
+        Map<String, List<Order>> orders = queryOrderByAccountIds(Lists.newArrayList("Peter_3", "Liz_4", "Jay_5"));
+
+        System.out.println(JSON.toJSONString(orders, true));
     }
 }
