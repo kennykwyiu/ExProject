def9e2e90ac28260d64e79b9f05f6838994125a2
kenny <kenny.kwyiu@gmail.com>
2024-04-22 21:19:06 +0800

    L3 3-29 Stream - sortTrade()

--- src/test/java/kenny/stream/cases/CaseFive.java
+++ src/test/java/kenny/stream/cases/CaseFive.java
@@ -1,13 +1,17 @@
 package kenny.stream.cases;
 
+import com.alibaba.fastjson.JSON;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CaseFive {
     @Data
@@ -43,5 +47,24 @@ public class CaseFive {
         trades.add(new Trade(new BigDecimal(100),
                 LocalDateTime.now().plusSeconds(0),
                 100, "Company"));
+    }
+
+    @Test
+    public void sortTrade() {
+        List<Trade> sorted = trades.stream()
+                .sorted(Comparator.comparing(Trade::getPrice, Comparator.reverseOrder())
+                        .thenComparing(Trade::getTime)
+                        .thenComparing(Trade::getCount, Comparator.reverseOrder())
+                        .thenComparing(Trade::getType, (type1, type2) -> {
+                            if ("Company".equals(type1) && "individual".equals(type2)) {
+                                return -1;
+                            } else if ("Company".equals(type2) && "individual".equals(type1)) {
+                                return 1;
+                            } else {
+                                return 0;
+                            }
+                        }))
+                .collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(sorted, true));
     }
 }
