7684dfaa036d46b40e83f37bbd74a187a4337754
kenny <kenny.kwyiu@gmail.com>
2024-04-25 20:17:26 +0800

    L4 5-2 Optional

---
+++ src/test/java/kenny/guava/OptionalTest.java
@@ -0,0 +1,36 @@
+package kenny.guava;
+
+import org.checkerframework.checker.nullness.Opt;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalTest {
+    @Test
+    public void test() {
+        Optional.empty();
+        Optional.of("Peter");
+        Optional<Object> optional = Optional.ofNullable("null");
+
+//        System.out.println(optional.isPresent());
+        optional.ifPresent(System.out::println);
+
+        optional.orElse("wrongly reference");
+        optional.orElseGet(() -> {
+            return "custom wrongly reference";
+        });
+        optional.orElseThrow(() -> {
+            throw new RuntimeException("wrongly ref exception");
+        });
+    }
+
+    public static void stream(List<String> list) {
+//        list.stream().forEach(System.out::println);
+
+    }
+
+    public static void main(String[] args) {
+        stream(null);
+    }
+}
