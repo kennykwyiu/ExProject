0bdaadb4251574a85503469235c0c1bb38864660
kenny <kenny.kwyiu@gmail.com>
2024-04-26 19:40:58 +0800

    L5 5-3 ImmutableTest - it's not immutable

--- src/test/java/kenny/guava/ImmutableTest.java
+++ src/test/java/kenny/guava/ImmutableTest.java
@@ -1,4 +1,20 @@
 package kenny.guava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ImmutableTest {
+    public static  void test(List<Integer> list) {
+        list.remove(0);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        test(list);
+        System.out.println(list);
+    }
 }
