625e50a0ee3b76652302e65dd179042b4ffdecff
kenny <kenny.kwyiu@gmail.com>
2024-04-27 22:55:13 +0800

    L5 5-6 Sets

---
+++ src/test/java/kenny/guava/SetsTest.java
@@ -0,0 +1,14 @@
+package kenny.guava;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.Set;
+
+public class SetsTest {
+
+    private static final Set set1 = Sets.newHashSet(1, 2);
+    private static final Set set2 = Sets.newHashSet(4);
+
+
+}
