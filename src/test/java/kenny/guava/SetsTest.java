d4bf3948aac849e0d512e893045743d3be277f83
kenny <kenny.kwyiu@gmail.com>
2024-04-28 22:02:10 +0800

    L5 5-6 Sets union

--- src/test/java/kenny/guava/SetsTest.java
+++ src/test/java/kenny/guava/SetsTest.java
@@ -7,8 +7,13 @@ import java.util.Set;
 
 public class SetsTest {
 
-    private static final Set set1 = Sets.newHashSet(1, 2);
-    private static final Set set2 = Sets.newHashSet(4);
+    private static final Set set1 = Sets.newHashSet(1, 2, 3) ;
+    private static final Set set2 = Sets.newHashSet(4, 5, 6);
 
+    @Test
+    public void union() {
+        Set<Integer> set = Sets.union(set1, set2);
+        System.out.println(set);
+    }
 
 }
