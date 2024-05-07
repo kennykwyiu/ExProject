5cf3bd44e05fa9b0c57ea2ecf9ba03dca5200303
kenny <kenny.kwyiu@gmail.com>
2024-05-07 21:41:59 +0800

    L6 6-1 newFixedThreadPool

--- src/test/java/kenny/threadpool/ThreadVs.java
+++ src/test/java/kenny/threadpool/ThreadVs.java
@@ -2,7 +2,34 @@ package kenny.threadpool;
 
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadVs {
+
+    @Test
+    public void newHandle() throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        for (int request = 1; request <= 100; request++) {
+            threadPool.execute(() -> {
+                System.out.println("Start to convert");
+
+                try {
+                    // convert word to pdf: take a long time
+                    Thread.sleep(1000L * 30);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("Finished convert");
+            });
+        }
+
+        Thread.sleep(1000L * 1000);
+
+    }
+
 
     @Test
     public void oldHandle() throws InterruptedException {
