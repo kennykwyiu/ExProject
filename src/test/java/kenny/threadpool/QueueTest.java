c79d2a39ef6fe67abd74f6bfe831974ce7822470
kenny <kenny.kwyiu@gmail.com>
2024-05-08 20:18:56 +0800

    L6 6-5 SynchronousQueue

--- src/test/java/kenny/threadpool/QueueTest.java
+++ src/test/java/kenny/threadpool/QueueTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 public class QueueTest {
 
@@ -18,12 +19,34 @@ public class QueueTest {
     }
 
     @Test
-    public void test() throws InterruptedException {
+    public void linkedBlockingQueue() throws InterruptedException {
         LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(10);
 
         for (int i = 0; i < 20; i++) {
             queue.put(i);
             System.out.println("add value into queue: " + i);
         }
+    }
+    @Test
+    public void synchronousQueue() {
+        SynchronousQueue<Integer> queue = new SynchronousQueue<>();
+
+        new Thread(() -> {
+            try {
+                queue.put(1);
+                System.out.println("add successfully");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                queue.take();
+                System.out.println("remove successfully");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
