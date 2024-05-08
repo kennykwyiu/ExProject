39517aa762f984371d5488bc530da21083ed2e0d
kenny <kenny.kwyiu@gmail.com>
2024-05-08 20:14:08 +0800

    L6 6-5 LinkedBlockingQueue

--- src/test/java/kenny/threadpool/QueueTest.java
+++ src/test/java/kenny/threadpool/QueueTest.java
@@ -3,12 +3,23 @@ package kenny.threadpool;
 import org.junit.Test;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class QueueTest {
 
     @Test
     public void arrayBlockingQueue() throws InterruptedException {
         ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
+
+        for (int i = 0; i < 20; i++) {
+            queue.put(i);
+            System.out.println("add value into queue: " + i);
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(10);
 
         for (int i = 0; i < 20; i++) {
             queue.put(i);
